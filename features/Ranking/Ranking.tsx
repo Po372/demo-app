5d46c586397dbe6d92e2b34f1e2293a2f4264348
kotek <scpktk@gmail.com>
2025-06-03 22:24:12 +0900

    ボタンを二つに増やしました

--- features/Ranking/Ranking.tsx
+++ features/Ranking/Ranking.tsx
@@ -11,6 +11,11 @@ export default function Ranking() {
             <path d="M240-200h120v-240h240v240h120v-360L480-740 240-560v360Zm-80 80v-480l320-240 320 240v480H520v-240h-80v240H160Zm320-350Z" />
           </svg>
         </Link>
+        <Link href="/">
+          <svg xmlns="http://www.w3.org/2000/svg" height="32px" viewBox="0 -960 960 960" width="32px" fill="#fff">
+            <path d="M240-200h120v-240h240v240h120v-360L480-740 240-560v360Zm-80 80v-480l320-240 320 240v480H520v-240h-80v240H160Zm320-350Z" />
+          </svg>
+        </Link>
         <div>ランキング</div>
       </div>
       <div className="w-9/10 border-2 border-lime-600 rounded-2xl px-4 py-12">
