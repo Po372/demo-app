6ae3d2e44e584a9ae140901da95c25c3d14fba61
kotek <scpktk@gmail.com>
2025-06-03 23:41:35 +0900

    Rankingページのスタイルを修正・pc画面に対応

--- features/Ranking/Ranking.tsx
+++ features/Ranking/Ranking.tsx
@@ -4,10 +4,10 @@ import Link from "next/link";
 
 export default function Ranking() {
   return (
-    <div className="flex flex-col items-center gap-12 text-xl">
+    <div className="flex flex-col items-center gap-8 text-xl mb-8">
       {/* ランキングボタン */}
-      <div className="flex items-center px-8 py-4 justify-between w-full bg-cyan-600 rounded-b-3xl font-bold text-white">
-        <Link href="/">
+      <div className="flex items-center px-8 py-2 justify-between w-full bg-cyan-600 rounded-b-3xl font-bold text-white">
+        <Link href="/" className="p-2 rounded-full transition hover:bg-cyan-700">
           <svg xmlns="http://www.w3.org/2000/svg" height="32px" viewBox="0 -960 960 960" width="32px" fill="#fff">
             <path d="M240-200h120v-240h240v240h120v-360L480-740 240-560v360Zm-80 80v-480l320-240 320 240v480H520v-240h-80v240H160Zm320-350Z" />
           </svg>
@@ -15,7 +15,7 @@ export default function Ranking() {
         <div>ランキング</div>
       </div>
       {/* レア失敗ランキング */}
-      <div className="w-9/10 border-2 border-lime-600 rounded-2xl px-4 py-12">
+      <div className="w-9/10 border-2 border-lime-600 rounded-2xl px-4 py-12 md:w-3/10">
         <div className="text-center font-bold text-2xl">レア失敗ランキング</div>
         <div className="mt-12">1位</div>
         <div className="flex items-center justify-between">
@@ -25,7 +25,7 @@ export default function Ranking() {
         <div className="mt-12">全社員に退職通知を送付してしまった。</div>
       </div>
       {/* 普遍失敗ランキング */}
-      <div className="w-9/10 border-2 border-cyan-600 rounded-2xl px-4 py-12">
+      <div className="w-9/10 border-2 border-cyan-600 rounded-2xl px-4 py-12 md:w-3/10">
         <div className="text-center font-bold text-2xl">普遍失敗ランキング</div>
         <div className="mt-12">1位</div>
         <div className="flex items-center justify-between">
