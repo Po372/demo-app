d5d048e0fc9ff23608d2252ece2ec9578aa89999
Po <0da3875409x180d@au.com>
2025-06-04 22:08:10 +0900

    ランキングの見た目変更

--- features/Result/Result.tsx
+++ features/Result/Result.tsx
@@ -38,10 +38,12 @@ export const Result: React.FC<ResultProps> = ({ grade, score, ranking }) => {
                 やらかし度：<span className="font-semibold text-4xl">{score}点</span>
             </p>
                 
-                <div className="bg-lime-200 rounded-md mt-6 py-2 px-4 space-y-1 text-left text-black text-sm">
+                <div className="bg-lime-200 rounded-md mt-6 py-2 px-4 space-y-1 text-left text-black text-sm font-bold">
                 {ranking.map((entry) => (
                     <div key={entry.id}>
-                    {entry.rank}位 {entry.userName} {entry.score}点
+                      <span className="min-w-[60px] font-mono">
+                        {entry.rank}位 {entry.userName} {entry.score}点
+                      </span>
                     </div>
                 ))}
                 </div>
@@ -49,7 +51,7 @@ export const Result: React.FC<ResultProps> = ({ grade, score, ranking }) => {
     </div>
         {/* 🏆 ランキングボタン */}
         <Link href="/ranking">
-        <button className="hover:bg-zinc-700 transition-colors duration-200 mt-5 bg-black text-lime-400 font-bold py-3 px-15 shadow-md flex items-center gap-2">
+        <button className="hover:bg-zinc-700 transition-colors duration-200 mt-5 bg-black text-lime-400 font-bold py-3 px-15 shadow-md flex items-center gap-2 ">
             <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M280-120v-80h160v-124q-49-11-87.5-41.5T296-442q-75-9-125.5-65.5T120-640v-40q0-33 23.5-56.5T200-760h80v-80h400v80h80q33 0 56.5 23.5T840-680v40q0 76-50.5 132.5T664-442q-18 46-56.5 76.5T520-324v124h160v80H280Zm0-408v-152h-80v40q0 38 22 68.5t58 43.5Zm200 128q50 0 85-35t35-85v-240H360v240q0 50 35 85t85 35Zm200-128q36-13 58-43.5t22-68.5v-40h-80v152Zm-200-52Z"/></svg>
             ランキングを見る
         </button>
