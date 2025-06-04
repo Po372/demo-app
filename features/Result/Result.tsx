9e77c12462eacf6f5b3baebdabccf9991de81a99
Po <0da3875409x180d@au.com>
2025-06-04 21:41:58 +0900

    ランキングボタンにページ移動とカーソルを合わせたときに色を変える機能を追加

--- features/Result/Result.tsx
+++ features/Result/Result.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import Link from "next/link";
 import React from 'react';
 
@@ -30,7 +28,7 @@ export const Result: React.FC<ResultProps> = ({ grade, score, ranking }) => {
 
         {/* 📄 メインカード */}
         <div className="mt-4">
-            <div className="border-2 border-cyan-400 rounded-xl">
+            <div className=" border-3 border-cyan-400 rounded-xl ">
                 <p className="mt-4 text-gray-700 text-sm mb-2">あなたの結果は…</p>
                     <div className="text-6xl font-bold  w-24 h-24 flex items-center justify-center mx-auto my-4 text-black">
                         {grade}
@@ -50,11 +48,13 @@ export const Result: React.FC<ResultProps> = ({ grade, score, ranking }) => {
         </div>
     </div>
         {/* 🏆 ランキングボタン */}
-        <button className="mt-5 bg-black text-lime-400 font-bold py-3 px-15 shadow-md flex items-center gap-2">
+        <Link href="/ranking">
+        <button className="hover:bg-zinc-700 transition-colors duration-200 mt-5 bg-black text-lime-400 font-bold py-3 px-15 shadow-md flex items-center gap-2">
             <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M280-120v-80h160v-124q-49-11-87.5-41.5T296-442q-75-9-125.5-65.5T120-640v-40q0-33 23.5-56.5T200-760h80v-80h400v80h80q33 0 56.5 23.5T840-680v40q0 76-50.5 132.5T664-442q-18 46-56.5 76.5T520-324v124h160v80H280Zm0-408v-152h-80v40q0 38 22 68.5t58 43.5Zm200 128q50 0 85-35t35-85v-240H360v240q0 50 35 85t85 35Zm200-128q36-13 58-43.5t22-68.5v-40h-80v152Zm-200-52Z"/></svg>
             ランキングを見る
         </button>
-        </div>
+        </Link>
+    </div>
     
   );
 };
