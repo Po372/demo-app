4b99c4a23cc5cb2d4014c38ae301c255d7f286e0
rikuto <rikuto042700@icloud.com>
2025-06-05 11:59:10 +0900

    ローカルサーバーに反映させました

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,5 @@
-import { Result } from "@/features/Result/Result";
+import Home from "@/features/Home/Home";
 
-const testRanking = [
-  { id: 1, userId: 1, userName: 'ゆかぽ', score: 97, rank: 172 },
-  { id: 2, userId: 2, userName: 'ミサイル', score: 127, rank: 173 },
-  { id: 3, userId: 3, userName: 'さいれん', score: 110, rank: 174 },
-];
-
-export default function Home() {
-  return (
-    <>
-      <div className="text-3xl">
-        <p>
-          これは最初のページです。
-        </p>
-      </div>
-      <div className="bg-white">
-        <Result grade="SS" score={127} ranking={testRanking}/>
-      </div>
-    </>
-  );
+export default function FirstPage() {
+  return <Home />;
 }
