b320c26a60dc444033c7ed57ac8e7b7b83a797b1
rikuto <rikuto042700@icloud.com>
2025-06-07 15:44:55 +0900

    見出しの部分の装飾をしました

--- features/Home/Headline.tsx
+++ features/Home/Headline.tsx
@@ -1,4 +1,18 @@
+import "./CSS/Headline.css";
+
 function Headline() {
-  return <h1>やらかしを入力して究極のやらかし度を目指せ！</h1>;
+  return (
+    <span
+      className="headline"
+      style={{
+        display: "inline-block",
+        padding: "0.25rem 0.5rem",
+        border: "1px solid rgb(61, 248, 14)",
+        borderRadius: "0.5rem",
+      }}
+    >
+      やらかしを入力して究極のやらかし度を目指せ！
+    </span>
+  );
 }
 export default Headline;
