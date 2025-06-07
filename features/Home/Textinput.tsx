ff08e2abcc0ac58eb744df238308cf1adf13c8ab
rikuto <rikuto042700@icloud.com>
2025-06-07 16:25:06 +0900

    入力されてないときに例を表示するようにしました

--- features/Home/Textinput.tsx
+++ features/Home/Textinput.tsx
@@ -1,5 +1,12 @@
+import "./CSS/TextInput.css";
 function TextInput() {
-  return <textarea></textarea>;
+  // テキスト入力+入力されてないときに例を表示するコンポーネント
+  return (
+    <textarea
+      className="text-input"
+      placeholder="例：カップ焼きそばに”かやく”を入れずにお湯を注いでしまった"
+    ></textarea>
+  );
 }
 //ステイト必須かも？
 
