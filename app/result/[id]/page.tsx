036beddeefb8ae3c36f7dd8383445ebfe1cf03b8
Po <0da3875409x180d@au.com>
2025-06-08 14:52:00 +0900

    いらんものを削除

--- app/result/[id]/page.tsx
+++ app/result/[id]/page.tsx
@@ -119,19 +119,3 @@ export default function HomeResultPage() {
     
   );
 }
-
-/*過去の遺物
-const testRanking = [
-  { id: '1', userId: 1, userName: 'ゆかぽ',comment: 'なし', score: 97, rank: 172 },
-  { id: '2', userId: 2, userName: 'ミサイル',comment: 'なし', score: 127, rank: 173 },
-  { id: '3', userId: 3, userName: 'さいれん',comment: 'なし', score: 110, rank: 174 },
-];
-
-export default function Home() {
-  return (
-    <div className="bg-white">
-      <Result grade="SS" score={127} ranking={testRanking}/>
-    </div>
-  );
-}
-*/
