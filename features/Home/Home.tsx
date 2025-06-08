2ab58f5793fd713f0d057bae51d1da66b52ed329
kotek <scpktk@gmail.com>
2025-06-08 13:15:31 +0900

    ユーザ名変更ボタンを実装

--- features/Home/Home.tsx
+++ features/Home/Home.tsx
@@ -2,6 +2,8 @@ import "../Home/CSS/Home.css";
 import Ranking from "./Ranking";
 import Scoring from "./Scoring";
 import HeadlineInput from "./HeadlineInput";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Home({
   error,
@@ -14,8 +16,30 @@ export default function Home({
   setInputFailureText: (text: string) => void;
   onScoringClick: () => void;
 }) {
+  const [userName, setUserName] = useState<string | null>(null);
+
+  useEffect(() => {
+    // ローカルストレージからユーザー名を取得
+    const storedUserName = localStorage.getItem("userName");
+    if (storedUserName) {
+      setUserName(storedUserName);
+    }
+  }, []);
+
   return (
     <div className="home-container">
+      <Link
+        href="/user-name"
+        className="flex absolute top-4 left-4 text-white text-l gap-2 justify-center items-center p-4 rounded-full bg-cyan-600 hover:bg-cyan-700 transition"
+      >
+        <div className="flex justify-center items-center text-cyan-800 font-bold text-xl rounded-full w-12 h-12 bg-white">
+          {userName?.charAt(0)}
+        </div>
+        <div className="flex flex-col">
+          <div className="text-sm">{userName ? `${userName}さん` : "ユーザー名を設定"}</div>
+          <div className="text-white/60 text-xs">クリックしてユーザ名を変更</div>
+        </div>
+      </Link>
       <Ranking />
       <HeadlineInput inputFailureText={inputFailureText} setInputFailureText={setInputFailureText} />
       {error && (
